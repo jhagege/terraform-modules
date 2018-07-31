e13e5d94bdea9de7a951c7f0c4dd18d79ab259fb
joachim.hagege <joachim.hagege@alibaba-inc.com>
2018-08-01 00:05:42 +0300

    Initial commit of modules repo

---
+++ services/webserver-cluster/vars.tf
@@ -0,0 +1,29 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
+}
+
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket used for the database's remote state storage"
+  default = "terraform-up-and-running-state-jo"
+}
+
+variable "db_remote_state_key" {
+  description = "The name of the key in the S3 bucket used for the database's remote state storage"
+}
+
+variable "instance_type" {
+  description = "The type of EC2 Instances to run (e.g. t2.micro)"
+}
+
+variable "min_size" {
+  description = "The minimum number of EC2 Instances in the ASG"
+}
+
+variable "max_size" {
+  description = "The maximum number of EC2 Instances in the ASG"
+}
